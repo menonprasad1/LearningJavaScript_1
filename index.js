8c01916a8c78b38ba0e9da8cfd17342cf5e6f951
menonprasad <pmenon@hortonworks.com>
2022-12-03 09:26:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//create a new inbuild object array using new and invoking the constructor.
+let arr_obj1 = new Array(1,4,3,2);
+let arr_obj2 = new Array(6,7,8)
+//Arraj concatenate example: Creates a new Array object, existing Array objected not changed.
+arr_obj_concat_obj1_obj2 = arr_obj1.concat(arr_obj2)
+console.debug("Array concatenation Example")
+console.debug(arr_obj_concat_obj1_obj2)
+//Create a new Date object, using the Date() Constructor
+let date_obj=new Date()
+console.debug("Create a new Date object - It shows the current time")
+console.debug(date_obj)
+//Create an Empty Object
+console.debug("Create a new Empty Object")
+let emp_obj = new Object()
+console.debug(emp_obj)
